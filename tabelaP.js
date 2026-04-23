17dd4531976be80e24a5bdc1910b2f6c61dc8aff
Vagner <kadrak02@hotmail.com.com>
2026-04-23 15:00:40 -0300

    'deploy'

---
+++ tabelaP.js
@@ -0,0 +1,13 @@
+const tabelaP = {
+  1: {
+    filtraPor: 'AL.',
+    id: 2,
+    idRR: '24',
+    nome: 'lata',
+    precoCompra: '8.50',
+    precoVenda: '9.50',
+    sort: '1',
+  },
+};
+
+export default tabelaP;
